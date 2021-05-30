cda2843579a9f34705f7ad547b234157bcaf8cef
ijbonn <bonnerij@gmail.com>
2021-05-30 09:48:54 -0600

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'localhost',
+  host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs290_bonneri',
   password        : '7171',
   database        : 'cs290_bonneri'
